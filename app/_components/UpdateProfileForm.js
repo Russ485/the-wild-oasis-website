6b9a4f661588b128f3100371d1ee3ae739152b29
Russ485 <kazyonnylawyer@gmail.com>
2025-09-20 11:09:11 +0200

    fixed empty flag state issue

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -34,11 +34,15 @@ async function UpdateProfileForm({ guest, children }) {
       <div className="space-y-2">
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
-          <img
-            src={countryFlag}
-            alt="Country flag"
-            className="h-5 rounded-sm"
-          />
+          {countryFlag ? (
+            <img
+              src={countryFlag}
+              alt="Country flag"
+              className="h-5 rounded-sm"
+            />
+          ) : (
+            <span>No country selected</span>
+          )}
         </div>
         {children}
       </div>
