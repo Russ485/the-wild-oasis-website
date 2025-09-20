4286841f8a81326efb5da8a727ec39947e767ec1
Russ485 <kazyonnylawyer@gmail.com>
2025-09-20 10:53:15 +0200

    fixed nesseccary validation user update form

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -49,6 +49,7 @@ async function UpdateProfileForm({ guest, children }) {
           name="nationalID"
           defaultValue={nationalID}
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+          required
         />
       </div>
 
